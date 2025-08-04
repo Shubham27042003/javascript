956f712051ebc694005716f84534cc526f79d40c
ShubhamL27 <shubham.lanke03@gmail.com>
2025-08-04 12:40:09 +0530

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=123445
+let accountEmail="shubhamlanke@gmail.com"
+var accountPassword="12345"
+accountCity="mumbai"
+let accountState
+//accountId=1235 //not allowed
+accountEmail="sl@gmail.com"
+accountPassword="2542"
+accountCity="bengalluru"
+console.log(accountId)
+/*
+prefer not to use var
+because of functional and block scope
+ */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"//treat all js code as newer version
+// alert(3+3) we are using node js not browser
+console.log(3
+    +3
+) //avoid this code readability should be high
+let name="shubham"
+let age=18
+let isSave=true
+let state
+//number =>2 to power53
+//bigint
+//string=>""
+//boolean=true/false
+//null standalone value
+//undefined=>
+    //symbol =>unique
+// object
+console.log(typeof isSave)
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
