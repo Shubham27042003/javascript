50fdbfe350011a8a327bc867bda861b689e4d2dc
ShubhamL27 <shubham.lanke03@gmail.com>
2025-08-04 14:45:06 +0530

    conversion done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score="hitessh"
+console.log(typeof score)
+let valueNumber=Number(score)
+console.log(typeof valueNumber)
+console.log(valueNumber)
+//"33"=>33
+//"hiesh123"=>NAN
+//true=>1 false=>0
+let isLoggedIn="shu"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+//1=>true 0=>false
+//""=>false
+//"shubham"=>true
+let someNumber=33
+let StringNumber=String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
+
